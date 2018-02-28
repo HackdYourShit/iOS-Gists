4abc9488ac17413f4287f927abb8f3b8c5b1530d
Najdan Tomić <najdan.tomic@gmail.com>
2018-02-28 13:08:31 +0100

    Create UIWindowExtension.swift

---
+++ UIWindowExtension.swift
@@ -0,0 +1,30 @@
+
+extension UIWindow {
+
+	/// Changes rootViewController to specified one.
+	///
+	/// - Parameters:
+	///   - viewController: new rootViewController
+	///   - animated: will switch have animation
+	///   - duration: duration of animation
+	///   - options: animation options
+	///   - completion: completion callback when switch finished
+	func switchRootViewController(_ viewController: UIViewController, animated: Bool = true, duration: TimeInterval = 0.5, options: UIViewAnimationOptions = .transitionFlipFromRight, completion: (() -> Void)? = nil) {
+		removeAllSubviews()
+		if animated {
+			UIView.transition(with: self,
+							  duration: duration,
+							  options: options,
+							  animations: {
+								let oldState = UIView.areAnimationsEnabled
+								UIView.setAnimationsEnabled(false)
+								self.rootViewController = viewController
+								UIView.setAnimationsEnabled(oldState) },
+							  completion: { _ in completion?() }
+			)
+		} else {
+			rootViewController = viewController
+			completion?()
+		}
+	}
+}
