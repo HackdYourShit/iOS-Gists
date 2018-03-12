17e42373edf7b88eb3e88afbf2a842c8136f613a
Najdan Tomić <najdan.tomic@gmail.com>
2018-03-12 20:16:54 +0100

    Update Is Keyboard Presented.swift

--- Is Keyboard Presented.swift
+++ Is Keyboard Presented.swift
@@ -4,10 +4,10 @@ extension UIApplication {
 	/// Checks if view hierarchy of application contains `UIRemoteKeyboardWindow` if it does, keyboard is presented
 	var isKeyboardPresented: Bool {
 		if let keyboardWindowClass = NSClassFromString("UIRemoteKeyboardWindow"), self.windows.contains(where: { $0.isKind(of: keyboardWindowClass) }) {
-			Log.info("UIApplication windows contains UIRemoteKeyboardWindow - keyboard is presented")
+			NSLog("UIApplication windows contains UIRemoteKeyboardWindow - keyboard is presented")
 			return true
 		} else {
-			Log.info("UIApplication windows does not contains UIRemoteKeyboardWindow - keyboard is not presented")
+			NSLog("UIApplication windows does not contains UIRemoteKeyboardWindow - keyboard is not presented")
 			return false
 		}
 	}
